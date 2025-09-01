79b7e0a140394ef35649c04147dfeb42e0943a9d
Kimsuro <ss01056489656@gmail.com>
2025-09-01 22:15:18 +0900

    axios 인스턴스 생성

--- Frontend/src/api/axios/index.js
+++ Frontend/src/api/axios/index.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 // axios 인스턴스 설정
+const apiClient = axios.create({
+  baseURL: "http://localhost:3001",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiClient;
