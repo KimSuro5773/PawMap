1df6e435994c57c96f1295af54e657131ee184f1
Kimsuro <ss01056489656@gmail.com>
2025-09-06 15:51:49 +0900

    Tanstack Query defaultOptions 설정
    stale, cache 설정 안할시 5분, 10분

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,17 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5, // 5분
+      cacheTime: 1000 * 60 * 10, // 10분
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
