1b1889a27592074881dcf5e2dba05871862c78fe
Suro Kim <ss01056489656@gmail.com>
2025-09-09 10:35:32 +0900

    TourAPI 기본 뼈대 작성

--- Frontend/src/api/services/tourApi.js
+++ Frontend/src/api/services/tourApi.js
@@ -1,1 +1,23 @@
 // 한국관광공사 TourAPI 관련 함수들
+
+export const getAreaCode = async () => {}; // 지역코드조회
+
+export const getCategoryCode = async () => {}; // 서비스 분류코드 조회
+
+export const getAreaBasedList = async () => {}; // 지역기반 관광정보 조회
+
+export const getLocationBasedList = async () => {}; // 위치기반 관광정보 조회
+
+export const getSearchKeyword = async () => {}; // 키워드 검색 조회
+
+export const getDetailCommon = async () => {}; // 공통 정보 조회
+
+export const getDetailIntro = async () => {}; // 소개 정보 조회
+
+export const getDetailInfo = async () => {}; // 반복 정보 조회
+
+export const getDetailImage = async () => {}; // 이미지 정보 조회
+
+export const getDetailPetTour = async () => {}; // 반려동물 동반여행 조회
+
+export const getPetTourSyncList = async () => {}; // 반려동물 동반여행 동기화 목록조회
