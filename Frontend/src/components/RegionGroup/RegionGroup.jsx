4efe60e12897566b570af43ad41b97a752e77acb
Kimsuro <ss01056489656@gmail.com>
2025-09-10 13:35:52 +0900

    홈 화면에 쓰일 국내 지역 RegionGroup components 생성

---
+++ Frontend/src/components/RegionGroup/RegionGroup.jsx
@@ -0,0 +1,9 @@
+import styles from "./RegionGroup.module.scss";
+
+export default function RegionGroup() {
+  return (
+    <div className={styles.regionGroup}>
+      <h2>국내 여행지</h2>
+    </div>
+  );
+}
