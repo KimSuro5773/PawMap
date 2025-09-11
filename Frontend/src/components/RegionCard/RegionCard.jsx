c80d9c21906c45edec08d640447a54ebd0d6e009
Kimsuro <ss01056489656@gmail.com>
2025-09-12 01:36:32 +0900

    국내 여행지 슬라이더 크기 변경

--- Frontend/src/components/RegionCard/RegionCard.jsx
+++ Frontend/src/components/RegionCard/RegionCard.jsx
@@ -1,24 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./RegionCard.module.scss";
 
-export default function RegionCard({ 
-  name, 
-  areaCode, 
-  image, 
-  className 
-}) {
+export default function RegionCard({ name, areaCode, image, className }) {
   return (
-    <Link 
+    <Link
       to={`/search?region=${areaCode}&name=${name}`}
-      className={`${styles.regionCard} ${className || ''}`}
+      className={`${styles.regionCard} ${className || ""}`}
     >
       <div className={styles.imageContainer}>
         {image ? (
-          <img 
-            src={image} 
-            alt={name} 
-            className={styles.image}
-          />
+          <img src={image} alt={name} className={styles.image} loading="lazy" />
         ) : (
           <div className={styles.placeholder}>
             <span>{name.charAt(0)}</span>
--- Frontend/src/data/regions.js
+++ Frontend/src/data/regions.js
@@ -1,54 +1,125 @@
 import { AREA_CODES, AREA_CODE_NAMES } from "../api/types/tour";
 
-// 국내 여행지 데이터 (이미지와 설명 추가)
+// 국내 여행지 데이터 (로컬 이미지와 설명)
 export const DOMESTIC_REGIONS = [
-  {
-    id: "jeju",
-    name: AREA_CODE_NAMES[AREA_CODES.JEJU],
-    areaCode: AREA_CODES.JEJU,
-    image:
-      "https://images.unsplash.com/photo-1578662996442-48f60103fc96?w=400&h=400&fit=crop",
-    description: "아름다운 자연과 함께하는 반려동물 여행",
-  },
-  {
-    id: "busan",
-    name: AREA_CODE_NAMES[AREA_CODES.BUSAN],
-    areaCode: AREA_CODES.BUSAN,
-    image:
-      "https://images.unsplash.com/photo-1540607807351-c8e16ed5c13c?w=400&h=400&fit=crop",
-    description: "바다와 도시의 매력이 있는 항구도시",
-  },
-  {
-    id: "gyeongbuk",
-    name: AREA_CODE_NAMES[AREA_CODES.GYEONGBUK],
-    areaCode: AREA_CODES.GYEONGBUK,
-    image:
-      "https://images.unsplash.com/photo-1578474846511-04ba529f0b88?w=400&h=400&fit=crop",
-    description: "천년 고도 경주의 역사와 문화",
-  },
   {
     id: "seoul",
     name: AREA_CODE_NAMES[AREA_CODES.SEOUL],
     areaCode: AREA_CODES.SEOUL,
-    image:
-      "https://images.unsplash.com/photo-1565992441121-4367c2967103?w=400&h=400&fit=crop",
+    image: "/images/서울.jpg",
     description: "다양한 반려동물 친화 시설이 있는 수도",
-  },
-  {
-    id: "gyeonggi",
-    name: AREA_CODE_NAMES[AREA_CODES.GYEONGGI],
-    areaCode: AREA_CODES.GYEONGGI,
-    image:
-      "https://images.unsplash.com/photo-1592750475338-74b7b21085ab?w=400&h=400&fit=crop",
-    description: "수도권 근교의 자연과 휴식",
   },
   {
     id: "incheon",
     name: AREA_CODE_NAMES[AREA_CODES.INCHEON],
     areaCode: AREA_CODES.INCHEON,
-    image:
-      "https://images.unsplash.com/photo-1568019959450-9113ff3d7d00?w=400&h=400&fit=crop",
+    image: "/images/인천.jpg",
     description: "공항과 항만이 있는 관문도시",
+  },
+  {
+    id: "daejeon",
+    name: AREA_CODE_NAMES[AREA_CODES.DAEJEON],
+    areaCode: AREA_CODES.DAEJEON,
+    image: "/images/대전.jpg",
+    description: "과학과 교육의 중심 도시",
+  },
+  {
+    id: "daegu",
+    name: AREA_CODE_NAMES[AREA_CODES.DAEGU],
+    areaCode: AREA_CODES.DAEGU,
+    image: "/images/대구.jpg",
+    description: "영남의 중심 도시",
+  },
+  {
+    id: "gwangju",
+    name: AREA_CODE_NAMES[AREA_CODES.GWANGJU],
+    areaCode: AREA_CODES.GWANGJU,
+    image: "/images/광주.JPG",
+    description: "예술과 문화의 고장",
+  },
+  {
+    id: "busan",
+    name: AREA_CODE_NAMES[AREA_CODES.BUSAN],
+    areaCode: AREA_CODES.BUSAN,
+    image: "/images/부산.jpg",
+    description: "바다와 도시의 매력이 있는 항구도시",
+  },
+  {
+    id: "ulsan",
+    name: AREA_CODE_NAMES[AREA_CODES.ULSAN],
+    areaCode: AREA_CODES.ULSAN,
+    image: "/images/울산.jpg",
+    description: "산업과 자연이 조화를 이루는 도시",
+  },
+  {
+    id: "sejong",
+    name: AREA_CODE_NAMES[AREA_CODES.SEJONG],
+    areaCode: AREA_CODES.SEJONG,
+    image: "/images/세종.jpg",
+    description: "균형발전을 이끄는 행정수도",
+  },
+  {
+    id: "gyeonggi",
+    name: AREA_CODE_NAMES[AREA_CODES.GYEONGGI],
+    areaCode: AREA_CODES.GYEONGGI,
+    image: "/images/경기.JPG",
+    description: "수도권 근교의 자연과 휴식",
+  },
+  {
+    id: "gangwon",
+    name: AREA_CODE_NAMES[AREA_CODES.GANGWON],
+    areaCode: AREA_CODES.GANGWON,
+    image: "/images/강원.jpg",
+    description: "아름다운 산과 바다가 있는 청정지역",
+  },
+  {
+    id: "chungbuk",
+    name: AREA_CODE_NAMES[AREA_CODES.CHUNGBUK],
+    areaCode: AREA_CODES.CHUNGBUK,
+    image: "/images/충북.jpg",
+    description: "내륙 중심부의 자연과 역사",
+  },
+  {
+    id: "chungnam",
+    name: AREA_CODE_NAMES[AREA_CODES.CHUNGNAM],
+    areaCode: AREA_CODES.CHUNGNAM,
+    image: "/images/충남.jpg",
+    description: "서해안의 풍부한 자연과 온천",
+  },
+  {
+    id: "gyeongbuk",
+    name: AREA_CODE_NAMES[AREA_CODES.GYEONGBUK],
+    areaCode: AREA_CODES.GYEONGBUK,
+    image: "/images/경북.jpg",
+    description: "천년 고도 경주의 역사와 문화",
+  },
+  {
+    id: "gyeongnam",
+    name: AREA_CODE_NAMES[AREA_CODES.GYEONGNAM],
+    areaCode: AREA_CODES.GYEONGNAM,
+    image: "/images/경남.jpg",
+    description: "산과 바다를 모두 즐길 수 있는 지역",
+  },
+  {
+    id: "jeonbuk",
+    name: AREA_CODE_NAMES[AREA_CODES.JEONBUK],
+    areaCode: AREA_CODES.JEONBUK,
+    image: "/images/전북.jpg",
+    description: "전통 문화와 맛의 고장",
+  },
+  {
+    id: "jeonnam",
+    name: AREA_CODE_NAMES[AREA_CODES.JEONNAM],
+    areaCode: AREA_CODES.JEONNAM,
+    image: "/images/전남.jpg",
+    description: "섬과 갯벌의 아름다운 자연경관",
+  },
+  {
+    id: "jeju",
+    name: AREA_CODE_NAMES[AREA_CODES.JEJU],
+    areaCode: AREA_CODES.JEJU,
+    image: "/images/제주.jpg",
+    description: "아름다운 자연과 함께하는 반려동물 여행",
   },
 ];
 
