6ae7625f0e12d94d4df311fb0ab2648cc979da0d
Suro Kim <ss01056489656@gmail.com>
2025-09-16 12:45:57 +0900

    슬라이더 이미지 생성(화질 개선 필요)

--- Frontend/src/components/MainSlider/MainSlider.jsx
+++ Frontend/src/components/MainSlider/MainSlider.jsx
@@ -1,7 +1,45 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/navigation";
+import "swiper/css/pagination";
 import styles from "./MainSlider.module.scss";
 
 export default function MainSlider() {
-  return <div className={styles.mainSliderContainer}></div>;
+  const slides = [
+    "/images/homeSlider1.webp",
+    "/images/homeSlider2.webp",
+    "/images/homeSlider3.webp",
+    "/images/homeSlider4.webp",
+  ];
+
+  return (
+    <div className={styles.mainSliderContainer}>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        slidesPerView={1}
+        spaceBetween={0}
+        pagination={{
+          clickable: true,
+          bulletClass: styles.swiperBullet,
+          bulletActiveClass: styles.swiperBulletActive,
+        }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        className={styles.swiper}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index} className={styles.swiperSlide}>
+            <img
+              src={slide}
+              alt={`슬라이드 ${index + 1}`}
+              className={styles.slideImage}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
